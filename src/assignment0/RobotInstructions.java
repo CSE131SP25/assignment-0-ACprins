59f26b46bdf0c5ec07a0f86ea14cd4426ffc260c
ameliaprins <ameliaprins@localhost>
2025-01-20 15:39:07 -0600

    Tree drawing

--- src/assignment0/RobotInstructions.java
+++ src/assignment0/RobotInstructions.java
@@ -29,11 +29,75 @@ public class RobotInstructions  {
 		 * You will add instructions at the end of the code to cause the support.robot to move
 		 * as you desire.
 		 */
-		robot.forward(60);
+		robot.forward(400);
 		robot.turnLeft(90);
-		robot.forward(23);
-		robot.turnRight(90);
+		robot.forward(2);
+		robot.turnLeft(89);
+		robot.forward(400);
+		robot.turnLeft(180);
+		robot.forward(100);
+		robot.turnLeft(180);
+		robot.turnRight(89);
+		robot.forward(160);
+		robot.turnRight(176);
+		robot.forward(320);
+		robot.turnLeft(176);
+		robot.forward(320);
+		
+
+		robot.turnRight(176);
+		robot.forward(315);
+		robot.turnLeft(176);
+		robot.forward(310);
+		
+
+		robot.turnRight(176);
+		robot.forward(310);
+		robot.turnLeft(176);
+		robot.forward(300);
+		
+
+		robot.turnRight(174);
+		robot.forward(300);
+		robot.turnLeft(174);
+		robot.forward(260);
+		
+
+		robot.turnRight(173);
+		robot.forward(260);
+		robot.turnLeft(173);
+		robot.forward(240);
+		
+
+		robot.turnRight(170);
+		robot.forward(200);
+		robot.turnLeft(170);
+		robot.forward(200);
+		
+
+		robot.turnRight(168);
+		robot.forward(180);
+		robot.turnLeft(168);
+		robot.forward(170);
+		
+
+		robot.turnRight(170);
+		robot.forward(140);
+		robot.turnLeft(168);
+		robot.forward(100);
+		
+		robot.turnRight(170);
+		robot.forward(100);
+		robot.turnLeft(168);
+		robot.forward(90);
+		
+		robot.turnRight(130);
 		robot.forward(80);
+		robot.turnRight(110);
+		robot.forward(90);
+
+		
+		
 	}
 
 }
